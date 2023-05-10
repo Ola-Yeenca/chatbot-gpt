63bcde855661a0d4b970581c10a6ae83b0b6099a
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2023-05-10 14:15:32 +0200

    bot created with gradio

--- chatbot.py
+++ chatbot.py
@@ -1,40 +1,36 @@
 import gradio
 import openai
-import os
 
-openai.api_key = os.getenv("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")
+openai.api_key = "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"
 
-message = []
+messages = [{"role": "system", "content": "You know everything"}]
+
 
 def chatbot(user_input, model="gpt-3.5-turbo"):
     try:
-        message.append({"role": "user", "content": user_input})  # Add the question to the message history
+        messages.append({"role": "user", "content": str(user_input)})  # Add the question to the message history
 
         # Generate a response from OpenAI API
         response = openai.Completion.create(
-            engine=model,
-            prompt=' '.join(message),
-            max_tokens=50,
-            temperature=0.7
-        )
+                engine=model,
+                prompt=' '.join([message['content'] for message in messages]),
+                max_tokens=50,
+                temperature=0.7
+            )
 
-        message.append(response.choices[0].text.strip())  # Add the generated response to the message history
+
+        messages.append(response.choices[0].text.strip())  # Add the generated response to the message history
         return response.choices[0].text.strip()  # Return the response
 
-    except openai.Error as e:
-        # Handle OpenAI API-specific errors
-        error_message = f"OpenAI API error: {str(e)}"
-        return error_message
-
     except Exception as e:
-        # Handle any other general exceptions
+        # Handle any exceptions, including OpenAI API errors
         error_message = f"An error occurred: {str(e)}"
         return error_message
 
 # Define the interface
 demo = gradio.Interface(fn=chatbot,
                         inputs=gradio.inputs.Textbox(lines=2, label="How can I assist you today?"),
-                        outputs=gradio.outputs.Textbox(lines=2),
+                        outputs=gradio.outputs.Textbox(),
                         title="Chat Bot"
                         )
 
